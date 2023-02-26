196df46a4aab215a507bd6d80db20f26f79e0451
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2023-02-26 17:26:05 +0800

    update theme

---
+++ scripts/bilicard-fixed.cjs
@@ -0,0 +1,42 @@
+'use strict';
+const bilicard_js = require('../source/cdn/js/bilicard.cjs')
+
+const micromatch = require('micromatch');
+
+function isMatch(path, patterns) { //From Hexo processor
+    if (!patterns) return false;
+    return micromatch.isMatch(path, patterns);
+}
+
+hexo.extend.tag.register("bilicard", async (args) => {
+    return await bilicard_js(args[0]);
+}, { async: true });
+
+hexo.extend.filter.register('after_post_render', (data) => {
+    //Hexo bilicard plugin,originally by MaxChang3, fixed by YidaozhanYa
+
+    //生成 skipRenderList 数组，并包含配置文件中的 skip_render
+    const skipRenderList = [
+        '**/*.js',
+        '**/*.css'
+    ];
+
+    var skip_render = hexo.config.skip_render;
+    if (Array.isArray(skip_render)) {
+        skipRenderList.push(...skip_render);
+    } else if (typeof skip_render === 'string') {
+        if (skip_render.length > 0) {
+            skipRenderList.push(skip_render);
+        }
+    }
+
+    //遍历 skipRenderList 并判断是否是跳过渲染的文件
+    for (var regexp of skipRenderList) {
+        if (isMatch(data.permalink, regexp)) {
+            return data;
+        }
+    }
+
+    data.content += '<link rel="stylesheet" href="/cdn/css/bilicard.css" type="text/css">';
+    return data;
+});
---
+++ source/cdn/js/bilicard.cjs
@@ -0,0 +1,55 @@
+(async () => {
+    const nunjucks = require('nunjucks');
+    const fetch = await import ('node-fetch')
+    const path = require('path');
+
+    nunjucks.configure({autoescape: true});
+
+    function convert(num) {
+        return (num >= 1E4) ? (num / 1E4).toFixed(1) + "万" : (num)
+    }
+
+    function toHHMMSS(second) {
+        return [Math.floor(second / 3600),
+            Math.floor(second / 60) % 60,
+            second % 60].map(
+            n => n.toString().padStart(2, 0)
+        ).join(':')
+    }
+
+    async function getVideoMessage(id) {
+        let vtype;
+        if (id.substring(0, 2).toLowerCase() == "bv") {
+            vtype = ["bvid", ""];
+        } else {
+            vtype = ["aid", "av"];
+            id = id.toLowerCase().replace("av", "");
+        }
+        let data = (await (await fetch(`http://api.bilibili.com/x/web-interface/view?${vtype[0]}=${id}`)).json())['data'];
+        let strings;
+        if (data != undefined) {
+            strings = {
+                v_id: vtype[1] + id,
+                v_title: data['title'],
+                v_time: toHHMMSS(data['duration']),
+                v_playview: convert(data['stat']['view']),
+                v_danmaku: convert(data['stat']['danmaku']),
+                v_type: "视频",
+                v_upname: data['owner']['name'],
+                v_cover: data['pic']
+            };
+        } else {
+            strings = ({
+                v_id: vtype[1] + id,
+                v_title: "出错了！",
+            });
+        }
+        return strings;
+    }
+
+    module.exports = async function createCard(id) {
+        let card = nunjucks.render(
+            path.resolve(__dirname, 'bilicard.njk'), await getVideoMessage(id));
+        return card;
+    }
+})();
