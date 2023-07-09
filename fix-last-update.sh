6e95d9d23812db8f439b674bd47f5d3f2c6d5e33
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2023-07-09 09:23:35 +0800

    Test

--- fix-last-update.sh
+++ fix-last-update.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+export TZ='Asia/Shanghai'
 git ls-files -z | while read -d '' path; do
-    # touch -d "$(git log -1 --format="@%ct" "$path")" "$path"
-    echo "$(git log -1 --format="%ct" "$path" | xargs -I{} date -d @{} "+%Y-%m-%d %H:%M:%S")" "$path"
+    touch -d "$(git log -1 --format="@%ct" "$path")" "$pth"
+    printf "$(git log -1 --format="%ct" "$path" | xargs -I{} date -d @{} "+%Y-%m-%d %H:%M:%S")"
+    printf \\t
+    echo "$path"
 done
