4d20e22300ceac1fb5c350c06f748ef3a0855baf
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2023-07-09 09:27:01 +0800

    Test

--- fix-last-update.sh
+++ fix-last-update.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 export TZ='Asia/Shanghai'
 git ls-files -z | while read -d '' path; do
-    touch -d "$(git log -1 --format="@%ct" "$path")" "$pth"
-    printf "$(git log -1 --format="%ct" "$path" | xargs -I{} date -d @{} "+%Y-%m-%d %H:%M:%S")"
-    printf \\t
-    echo "$path"
+    if [[ $path == source* ]]; then
+        touch -d "$(git log -1 --format="@%ct" "$path")" "$path"
+        printf "$(git log -1 --format="%ct" "$path" | xargs -I{} date -d @{} "+%Y-%m-%d %H:%M:%S")"
+        printf \\t
+        echo "$path"
+    fi
 done
