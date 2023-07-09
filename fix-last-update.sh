73c7ef2f55779fb7ead9c4f9868865dfea52fd51
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2023-07-09 16:09:51 +0800

    🚮 Remove dependency: hexo-util

--- fix-last-update.sh
+++ fix-last-update.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 export TZ='Asia/Shanghai'
 git ls-files -z | while read -d '' path; do
-    if [[ $path == source* ]]; then
-        touch -d "$(git log -1 --format="@%ct" "$path")" "$path"
+    if [[ $path == source/_posts/* ]]; then
+        if [[ "$(hostname)" != "yidaozhan-home" ]]; then
+            touch -d "$(git log -1 --format="@%ct" "$path")" "$path"
+        fi
         printf "$(git log -1 --format="%ct" "$path" | xargs -I{} date -d @{} "+%Y-%m-%d %H:%M:%S")"
         printf \\t
         echo "$path"
