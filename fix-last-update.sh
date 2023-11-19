ae3bb65a003c6f8b73341f1259db87b25869d7b6
Kirikaze Chiyuki <me@chyk.ink>
2023-11-19 12:35:04 +0800

    update theme

--- fix-last-update.sh
+++ fix-last-update.sh
@@ -2,7 +2,7 @@
 export TZ='Asia/Shanghai'
 git ls-files -z | while read -d '' path; do
     if [[ $path == source/_posts/* ]]; then
-        if [[ "$(hostname)" != "yidaozhan-home" ]]; then
+        if [[ "$(hostname)" != "chiyuki-home" ]]; then
             touch -d "$(git log -1 --format="@%ct" "$path")" "$path"
         fi
         printf "$(git log -1 --format="%ct" "$path" | xargs -I{} date -d @{} "+%Y-%m-%d %H:%M:%S")"
